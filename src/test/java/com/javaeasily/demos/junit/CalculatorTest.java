f4e38911b8b72fdde8e7b9e6ef1fcb4106d86fef
random-walkie <26143642+random-walkie@users.noreply.github.com>
2024-12-04 13:21:52 +0000

    Changed add method of class Calculator from static to instance, and re-arranged CalculatorTest class. Added @BeforeEach annotation. Added pr ivate calc variable and instantiated new Calculator() object in the setup method of CalculatorTest class

--- src/test/java/com/javaeasily/demos/junit/CalculatorTest.java
+++ src/test/java/com/javaeasily/demos/junit/CalculatorTest.java
@@ -1,19 +1,45 @@
 package com.javaeasily.demos.junit;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import com.javaeasily.demos.junit.Calculator;
 
 public class CalculatorTest {
+    // Also need to push the object into the test class itself so it
+    // can be accessed by its methods.
+    // Can do this by setting a private object to the class.
+    private Calculator calc; //
+    @BeforeEach
+    // This method will be executed before each test
+    // due to the annotation given above, to setup the test fixture:
+    // the set of objects of each test class before each test is run.
+    public void setup(){
+        // If a method is not static, it is necessary
+        // to first instantiate an object to access the method
+        calc = new Calculator();
+    }
     @Test
-    public void testAdd() {
+    public void testAddPositiveNumbers() {
         // arrange
         int a = 10;
         int b = 20;
         //act
-        int result = Calculator.add(a, b);
+        int result = calc.add(a, b);
         //assert
         Assertions.assertEquals(30, result);
+    }
+
+    @Test
+    public void testAddZerotoPositiveNumbers() {
+        // arrange
+        int a = 10;
+        int b = 0;
+        //act
+        int result = calc.add(a, b);
+        //assert
+        Assertions.assertEquals(10, result);
     }
 
 }
