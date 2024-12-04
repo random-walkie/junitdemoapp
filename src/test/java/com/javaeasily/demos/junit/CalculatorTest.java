530061d0d2984d74a83747fe7ad4aef3e3325ead
random-walkie <26143642+random-walkie@users.noreply.github.com>
2024-12-04 15:17:12 +0000

    expanded explantion of test fixtures in CalculatorTest class.

--- src/test/java/com/javaeasily/demos/junit/CalculatorTest.java
+++ src/test/java/com/javaeasily/demos/junit/CalculatorTest.java
@@ -15,6 +15,10 @@ public class CalculatorTest {
     // This method will be executed before each test
     // due to the annotation given above, to setup the test fixture:
     // the set of objects of each test class before each test is run.
+    // It is worthy of note that due to @BeforeEach annotation:
+    // 1. The setup method it is set up fresh each time
+    //  1.1. This instantiates a new object that the test fixture is composed of
+    // 2. The test itselt is also recreated each timeq
     public void setup(){
         // If a method is not static, it is necessary
         // to first instantiate an object to access the method
@@ -37,6 +41,7 @@ public class CalculatorTest {
         int a = 10;
         int b = 0;
         //act
+        //  check this during Debug mode, by setting breaking points within each method
         int result = calc.add(a, b);
         //assert
         Assertions.assertEquals(10, result);
